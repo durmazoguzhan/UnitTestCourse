d38d9775660112e94b429054859c09448aff22a5
durmazoguzhan <durmazoguzhan@yahoo.com>
2024-01-04 02:04:37 +0300

    add CalculatorTest class

---
+++ UnitTestCourse.Test/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using UnitTestCourse.App;
+using Xunit;
+
+namespace UnitTestCourse.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange
+            int firstNumber = 5;
+            int secondNumber = 3;
+            Calculator calculator = new();
+
+            // Act
+            int total = calculator.Add(firstNumber, secondNumber);
+
+            // Assert
+            Assert.Equal(8, total);
+
+        }
+    }
+}
