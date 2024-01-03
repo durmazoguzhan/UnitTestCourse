97c488a66a3baa8feaa36951f7e2eb90bf541fa6
durmazoguzhan <durmazoguzhan@yahoo.com>
2024-01-04 01:40:13 +0300

    add Calculator class

---
+++ UnitTestCourse.App/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace UnitTestCourse.App
+{
+    public class Calculator
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public int Subtract(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public int Divide(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
