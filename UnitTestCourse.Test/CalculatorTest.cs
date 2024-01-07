596bdb65e25ec6faa852bf546f3a8fcbbdf2777e
durmazoguzhan <durmazoguzhan@yahoo.com>
2024-01-07 18:02:19 +0300

    add SubtractTest method

--- UnitTestCourse.Test/CalculatorTest.cs
+++ UnitTestCourse.Test/CalculatorTest.cs
@@ -18,7 +18,17 @@ namespace UnitTestCourse.Test
 
             // Assert
             Assert.Equal(8, total);
+        }
 
+        [Theory]
+        [InlineData(1, 5, -4)]
+        [InlineData(9, 8, 1)]
+        [InlineData(3, 3, 0)]
+        public void SubtractTest(int firstNumber, int secondNumber, int expectedTotal)
+        {
+            Calculator calculator = new();
+            int actualTotal = calculator.Subtract(firstNumber, secondNumber);
+            Assert.Equal(expectedTotal, actualTotal);
         }
     }
 }
